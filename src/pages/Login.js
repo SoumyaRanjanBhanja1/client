7a0ebacfd7ce53c54e54bf1f6b84bfc4b36d1ea6
SoumyaRanjanBhana1 <soumyabhanja180@gmail.com>
2025-06-21 16:01:14 +0530

    commited for client

---
+++ src/pages/Login.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Container, TextField, Button, Typography, Box } from '@mui/material';
+import axios from 'axios';
+
+const Login = () => {
+  const [form, setForm] = useState({ email: '', password: '' });
+
+  const navigate=useNavigate();
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post('http://localhost:5000/api/auth/login', form);
+      console.log(res.data.token);
+      alert(`Welcome ${res.data.user.name}`);
+      localStorage.setItem('token', res.data.token);
+      axios.defaults.headers.common["Authorization"] = `Bearer ${res.data.token}`;
+      navigate("/");
+    } catch (err) {
+      alert(err.response?.data?.msg || 'Login failed');
+    }
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Box mt={5}>
+        <Typography variant="h4">Login</Typography>
+        <TextField fullWidth label="Email" name="email" margin="normal" onChange={handleChange} />
+        <TextField fullWidth label="Password" name="password" type="password" margin="normal" onChange={handleChange} />
+        <Button variant="contained" fullWidth sx={{ mt: 2 }} onClick={handleLogin}>Login</Button>
+      </Box>
+    </Container>
+  );
+};
+
+export default Login;
---
+++ src/pages/Signup.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Container, TextField, Button, Typography, Box } from '@mui/material';
+import axios from 'axios';
+
+const Signup = () => {
+  const [form, setForm] = useState({ name: '', email: '', password: '' });
+
+  const navigate=useNavigate();
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSignup = async () => {
+    try {
+      const res = await axios.post('http://localhost:5000/api/auth/signup', form);
+      console.log(res.data);
+      alert(res.data.msg);
+      navigate("/login")
+    } catch (err) {
+      alert(err.response?.data?.msg || 'Signup failed');
+    }
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Box mt={5}>
+        <Typography variant="h4">Signup</Typography>
+        <TextField fullWidth label="Name" name="name" margin="normal" onChange={handleChange} />
+        <TextField fullWidth label="Email" name="email" margin="normal" onChange={handleChange} />
+        <TextField fullWidth label="Password" name="password" type="password" margin="normal" onChange={handleChange} />
+        <Button variant="contained" fullWidth sx={{ mt: 2 }} onClick={handleSignup}>Signup</Button>
+      </Box>
+    </Container>
+  );
+};
+
+export default Signup;

569c8f13e458fccb36e42f26a96ee097b6ba7882
SoumyaRanjanBhana1 <soumyabhanja180@gmail.com>
2025-06-21 18:32:00 +0530

    package modified

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -5,25 +5,29 @@ import axios from 'axios';
 
 const Signup = () => {
   const [form, setForm] = useState({
-  name: '',
-  email: '',
-  password: ''
-});
+    name: '',
+    email: '',
+    password: ''
+  });
 
   const API = "https://server-1-a50z.onrender.com";
-
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
   const handleSignup = async () => {
+    if (!form.name || !form.email || !form.password) {
+      alert("Please fill in all fields");
+      return;
+    }
+
     try {
-      const res = await axios.post(`${API}/api/auth/signup`,form);
+      const res = await axios.post(`${API}/api/auth/signup`, form);
       console.log(res.data);
-      alert(res.data.msg);
-      navigate("/login")
+      alert(res.data.msg || "Signup successful");
+      navigate("/login");
     } catch (err) {
       alert(err.response?.data?.msg || 'Signup failed');
     }
@@ -33,10 +37,39 @@ const Signup = () => {
     <Container maxWidth="sm">
       <Box mt={5}>
         <Typography variant="h4">Signup</Typography>
-        <TextField fullWidth label="Name" name="name" value={form.name} margin="normal" onChange={handleChange} />
-        <TextField fullWidth label="Email" name="email" value={form.email} margin="normal" onChange={handleChange} />
-        <TextField fullWidth label="Password" name="password"  value={form.password} type="password" margin="normal" onChange={handleChange} />
-        <Button variant="contained" fullWidth sx={{ mt: 2 }} onClick={handleSignup}>Signup</Button>
+        <TextField
+          fullWidth
+          label="Name"
+          name="name"
+          margin="normal"
+          onChange={handleChange}
+          value={form.name}
+        />
+        <TextField
+          fullWidth
+          label="Email"
+          name="email"
+          margin="normal"
+          onChange={handleChange}
+          value={form.email}
+        />
+        <TextField
+          fullWidth
+          label="Password"
+          name="password"
+          type="password"
+          margin="normal"
+          onChange={handleChange}
+          value={form.password}
+        />
+        <Button
+          variant="contained"
+          fullWidth
+          sx={{ mt: 2 }}
+          onClick={handleSignup}
+        >
+          Signup
+        </Button>
       </Box>
     </Container>
   );
